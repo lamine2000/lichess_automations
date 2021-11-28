22a2bce29a7560ffd197929d1bd5136389f965c3
Mouhamadou Lamine Gueye <lemzogueye.lg72@gmail.com>
2021-11-28 07:00:04 +0000

    adding 'responsetype' to the 'Access-Control-Allo-Headers' header

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 //CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin, Authorization");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin, Authorization, responsetype");
     next();
 });
 
